f58b84b647517ac6e02594c26be1c447dcf188af
Mukola-P <proxorov.mukola@gmail.com>
2022-10-17 16:42:14 +0300

    hw__10

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/api';
+import getRefs from './js/refs';
+import {
+  renderMarkup,
+  clearCountriesList,
+  clearCountry,
+} from './js/render-markup';
+
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+const { input } = getRefs();
+
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+  const searchQuery = e.target.value.trim();
+
+  if (!searchQuery) {
+    clearCountriesList();
+    clearCountry();
+    return;
+  }
+  fetchCountries(searchQuery).then(renderMarkup);
+}
---
+++ src/js/api.js
@@ -0,0 +1,18 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const searchParams = new URLSearchParams({
+  fields: 'name,capital,flags,population,languages',
+});
+
+export function fetchCountries(query) {
+  return fetch(`${BASE_URL}${query}?${searchParams}`)
+    .then(response => {
+      if (!response.ok) {
+        Notify.failure('Oops, there is no country with that name');
+        return new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.err(error));
+}
